130ddfd776044603ece5b8f5ed6e4773389c5b26
Sergey <sergeileonov@inbox.ru>
2023-04-15 10:51:55 +0100

    Dz3 Teacher

---
+++ src/main/java/Teacher/TeacherInterator.java
@@ -0,0 +1,24 @@
+package Teacher;
+import java.util.Iterator;
+import java.util.List;
+
+public class TeacherIterator implements Iterator<Teacher> {
+
+    private List<Teacher> teachers;
+    private int currentPosition;
+
+    public TeacherIterator(List<Teacher> teachers) {
+        this.teachers = teachers;
+        currentPosition = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return currentPosition < teachers.size();
+    }
+
+    @Override
+    public Teacher next() {
+        return teachers.get(currentPosition++);
+    }
+}
